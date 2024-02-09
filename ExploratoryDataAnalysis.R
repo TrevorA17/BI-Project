097a224bb147e46a37f0ca9d4106eb8a9b9944a7
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-02-09 17:32:01 +0300

    Load dataset

---
+++ ExploratoryDataAnalysis.R
@@ -0,0 +1,14 @@
+#Load dataset
+TitanicData <-read.csv("data/titanic.csv", colClasses = c(
+  PassengerId = "numeric",
+  Pclass = "numeric",
+  Name = "character",
+  Sex = "factor",
+  Age = "numeric",
+  SibSp = "numeric",
+  Parch = "numeric",
+  Ticket = "character",
+  Fare = "numeric",
+  Cabin = "character",
+  Embarked = "factor"
+))
